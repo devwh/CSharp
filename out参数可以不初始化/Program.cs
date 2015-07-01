c0b6cbcdc144912f1e8825d9a17c3599dee7d426
devwh <dev_wh@163.vom>
2015-07-01 15:20:19 +0800

    out参数可以不初始化

---
+++ out参数可以不初始化/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace out参数可以不初始化
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine(func(out a));
+            Console.WriteLine(a);
+        }
+
+        static int func(out int a)
+        {
+            a = 100;
+            return a;
+        }
+    }
+}
