11e1369cb37223a24aafc9b8fec2d91f3deef5a8
devwh <dev_wh@163.vom>
2015-07-17 09:26:20 +0800

    内存管理和指针

---
+++ dynamic类型/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 给dynamic类型定义的任何操作都是有效的，运行期检测错误
+ * dynamic对象可以在运行期改变类型
+ */
+namespace dynamic类型
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var staticPerson = new Person();
+            dynamic dynamicPerson = new Person();
+            // staticPerson.getFullName("Jone", "Smith"); // 编辑期报错
+            // dynamicPerson.getFullName("Jone", "Smith"); // 运行期报错
+
+            //改变类型
+            dynamic d = 100;
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(d);
+
+            d = "devwh";
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(d);
+
+            d = new Person();
+            Console.WriteLine(d.GetType());
+            Console.WriteLine(d);
+        }
+    }
+
+    class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string getFullName()
+        {
+            return string.Concat(firstName, " ", lastName);
+        }
+    }
+}
---
+++ 使用Python脚本/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 使用Python脚本
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string totalItems = "10";
+            string totalAmt = "2.34";
+            string scriptToUse;
+            bool costRadioButtonChecked = true;
+            if (costRadioButtonChecked)
+            {
+                scriptToUse = "AmountDisc.py";
+            }
+            else
+            {
+                scriptToUse = "CountDisc.py";
+            }
+            // 创建运行时
+            ScriptRuntime scriptRuntime = ScriptRuntime.CreateFromConfiguration();
+        }
+    }
+}
