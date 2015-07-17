1243ccf65ceaac8f111dc8f7792c82bb1a4e9594
devwh <dev_wh@163.vom>
2015-07-17 09:38:34 +0800

    IDisposable接口

---
+++ IDisposable接口/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposable接口
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = null;
+            try
+            {
+                myClass = new MyClass();
+                myClass.func();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (myClass != null)
+                {
+                    myClass.Dispose();
+                }
+            }
+
+            // 简写
+            using(MyClass s = new MyClass())
+            {
+                try
+                {                   
+                    s.func();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+
+    class MyClass : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("IDisposable接口");
+        }
+        public void func()
+        {
+            throw new Exception("a");
+        }
+    }
+}
---
+++ 析构函数/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 问题： 只有在垃圾回收时才会调用析构函数，而垃圾回收的时间不确定
+namespace 析构函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                MyClass myClass = new MyClass();
+            }
+        }
+    }
+
+    class MyClass
+    {
+        ~MyClass()
+        {
+            Console.WriteLine("MyClass 析构函数");
+        }
+    }
+}
